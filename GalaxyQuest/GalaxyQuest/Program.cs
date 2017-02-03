79d9f42190e7750f061702d514026eadc9a2bc7d
McKay Fenn <mckay.fenn@gmail.com>
2017-02-02 18:18:53 -0700

    progress on Galaxy Quest

---
+++ GalaxyQuest/GalaxyQuest/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GalaxyQuest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Star> allStars = new List<Star>();
+            Dictionary<long, long> stars = new Dictionary<long, long>();
+
+
+            long d;
+            int count = 0;
+            string line;
+            while ((line = Console.ReadLine()) != null)
+            {
+                string[] xy = line.Split(null);
+                if (count == 0)
+                {
+                    long.TryParse(xy[0], out d);
+                }
+                else
+                {
+                    
+                    long x;
+                    long y;
+                    long.TryParse(xy[0], out x);
+                    long.TryParse(xy[1], out y);
+                    //stars.Add(x, y);
+                    allStars.Add(new Star((x, y)));
+                }
+                count++; // this skips the first line
+            }
+
+            findMajority(allStars);
+        }
+
+        public static List<Star> findMajority(List<Star> A)
+        {
+            if (A.Count == 0)
+            {
+                return null;
+            }
+            else if (A.Count == 1)
+            {
+                return A;
+            }
+            else
+            {
+                List<Star> firstHalf = new List<Star>();
+                for (int i = 0; i < A.Count / 2; i++)
+                {
+                    firstHalf.Add(A.ElementAt(i));
+                }
+                List<Star> secondHalf = new List<Star>();
+                for (int i = A.Count / 2; i < A.Count; i++)
+                {
+                    secondHalf.Add(A.ElementAt(i));
+                }
+                List<Star> x = findMajority(firstHalf);
+                List<Star> y = findMajority(secondHalf);
+
+                if (x == null && y == null)
+                    return null;
+                else if (x == null)
+                {
+                    // count occurences of y in A, return y or NO
+                }
+                else if (y == null)
+                {
+                    // count occurences of x in A, return x or NO
+                }
+                else
+                {
+                    // count occurrences of x and y in A, return x, y, or NO
+                }
+            }
+
+            return null;
+        }
+
+        public long distance(Star s1, Star s2)
+        {
+            return Convert.ToInt64(Math.Pow(s1.x - s2.x, 2) + Math.Pow(s1.y - s2.y, 2));
+        }
+    }
+
+    class Star
+    {
+        public long x;
+        public long y;
+
+        Star(long _x, long _y)
+        {
+            x = _x;
+            y = _y;
+        }
+    }
+}
--- Merging-Searching-Arrays/ConsoleApplication1/Program.cs
+++ Merging-Searching-Arrays/ConsoleApplication1/Program.cs
@@ -15,17 +15,25 @@ namespace ConsoleApplication1
             int[] B = { 7, 8, 9, 14};
             int k = 5;
 
+            int answer = select(A, B, k);
+            Console.WriteLine(answer); // should be 9
+
             int[] C = { 22, 25, 31, 33, 48, 99, 104 };
             int[] D = { 1, 4, 23, 32, 55 };
-            //k = 8;
-
             // 1, 4, 22, 23, 25, 31, 32, 33, 48, 55, 99, 104
 
-            int answer = select(A, B, k);
-            Console.WriteLine("Searching A: " + " and B: " + ", for index: " + k + ". We get value: " + answer);
-
+            k = 8;
             answer = select(C, D, k);
-            Console.WriteLine(k);
+            Console.WriteLine(answer); // should be 48
+            
+
+            int[] E = { 7, 19, 24, 53, 55, 87 };
+            int[] F = { 1, 3, 5, 64, 74, 92 };
+            // 1, 3, 5, 7, 19, 24, 53, 55, 64, 74, 87, 92
+
+            k = 8;
+            answer = select(E, F, k);
+            Console.WriteLine(answer); 
 
             Console.ReadLine();
         }
@@ -51,16 +59,37 @@ namespace ConsoleApplication1
             int i = (loA + hiA) / 2;
             int j = (loB + hiB) / 2;
             // Figure out which one of four cases apply
+            //Console.WriteLine("A[i] = " + A[i] + ". B[j] = " + B[j]);
             if (A[i] > B[j])
-                if (k <= i + j) 
-                    return select(A, loA, hiA - 1, B, loB, hiB, k); // select(A, 0, A.Length - 1, B, 0, j, k);
-                else
-                    return select(A, loA, hiA, B, loB + 1, hiB, k); // select(A, 0, A.Length - 1, B, j, B.Length - 1, k);
-            else
+            {
                 if (k <= i + j)
-                    return select(A, loA, hiA, B, loB, hiB - 1, k); // select(A, 0, i, B, 0, B.Length - 1, k);
+                    return select(A, loA, i -1, B, loB, hiB, k); // throw away right half of A
+                else
+                    return select(A, loA, hiA, B, j + 1, hiB, k); // throw away left half of B
+            }
             else
-                    return select(A, loA + 1, hiA, B, loB, hiB, k); // select(A, i + 1, A.Length - 1, B, 0, B.Length - 1, k);
+            {
+                if (k <= i + j)
+                    return select(A, loA, hiA, B, loB, j - 1, k); // throw away right half of B
+                else
+                    return select(A, i +1, hiA, B, loB, hiB, k); // throw away left half of A
+            }
         }
     }
 }
+
+
+//if (A[i] > B[j])
+//{
+//    if (k <= i + j)
+//        return select(A, i - 1, hiA - 1, B, loB, hiB, k); // throw away right half of A
+//    else
+//        return select(A, loA, hiA, B, j + 1, hiB, k); // throw away left half of B
+//}
+//else
+//{
+//    if (k <= i + j)
+//        return select(A, loA, hiA, B, loB, j - 1, k); // throw away right half of B
+//    else
+//        return select(A, i + 1, hiA, B, loB, hiB, k); // throw away left half of A
+//}
