cbc5ac1c7a17c12feb495a26dc8ee157d50b6034
McKay Fenn <mckay.fenn@gmail.com>
2017-02-26 21:46:46 -0700

    passing test but it's (PQ) too slow

--- GetShorty/GetShorty/Program.cs
+++ GetShorty/GetShorty/Program.cs
@@ -12,32 +12,15 @@ namespace GetShorty
         {
             Program p = new Program();
 
-            List<Graph> list = new List<Graph>();
-
-            //List<Node2> graph = new List<Node2>();
 
 
 
             Dictionary<long, LinkedList<Node3>> graph = new Dictionary<long, LinkedList<Node3>>();
-            Dictionary<Node3, LinkedList<Node3>> graph2 = new Dictionary<Node3, LinkedList<Node3>>();
 
-            //8 13
-            //0 1 0.1
-            //1 2 0.2
-            //2 3 0.1
-            //0 5 0.8
-            //1 6 0.6
-            //3 6 0.1
-            //0 4 0.4
-            //1 5 0.6
-            //2 6 0.2
-            //3 7 0.4
-            //4 5 0.5
-            //6 5 0.1
-            //6 7 0.1
 
-            string[] testingData = { "3 3", "0 1 0.9", "1 2 0.9", "0 2 0.8" };
-            //string[] testingData = { "8 13", "0 1 0.1", "1 2 0.2", "2 3 0.1", "0 5 0.8", "1 6 0.6" };
+            //string[] testingData = { "3 3", "0 1 0.9", "1 2 0.9", "0 2 0.8" };
+            //string[] testingData = { "8 13", "0 1 0.1", "1 2 0.2", "2 3 0.1", "0 5 0.8", "1 6 0.6", "3 6 0.1", "0 4 0.4", "1 5 0.6", "2 6 0.2", "3 7 0.4", "4 5 0.5", "6 5 0.1", "6 7 0.1" };
+            string[] testingData = { "3 3", "0 1 0.9", "1 2 0.9", "0 2 0.8", "2 1", "1 0 1", "0 0" };
 
             string line;
             int lineCount = 0;
@@ -53,11 +36,20 @@ namespace GetShorty
                 string[] data = testingData.ElementAt(i).Split(null);
 
 
-                if (lineCount == 0)
+                if (data.Length == 2)
                 {
+                    if (lineCount > 0)
+                    {
+                        p.dijkstra2(graph, 0, numOfIntersections - 1);
+                    }
+                    if (int.Parse(data[0]) == 0 && int.Parse(data[1]) == 0)
+                    {
 
+                        break;
+                    }
                     numOfIntersections = int.Parse(data[0]);
                     numOfCorridors = int.Parse(data[1]);
+                    graph = new Dictionary<long, LinkedList<Node3>>();
                 }
                 else
                 {
@@ -79,32 +71,9 @@ namespace GetShorty
                     graph[first].AddLast(new Node3(first, second, factor));  // create the edges.
                     graph[second].AddLast(new Node3(second, first, factor)); // they are undirected, so create it both ways.
 
-
-                    //if (!graph2.ContainsKey(first))
-                    //{
-                    //    graph2.Add(first, new LinkedList<Node3>());
-                    //}
-                    //if (!graph2.ContainsKey(second))
-                    //{
-                    //    graph2.Add(second, new LinkedList<Node3>());
-                    //}
-                    //graph2[first].AddLast(new Node3(first, second, factor));  // create the edges.
-                    //graph2[second].AddLast(new Node3(second, first, factor)); // they are undirected, so create it both ways.
                 }
                 lineCount++;
             }
-
-            p.printGraph(graph);
-
-
-
-
-
-
-            p.dijkstra2(graph, 0, numOfIntersections - 1);
-
-
-           
 
             Console.ReadLine();
 
@@ -113,7 +82,7 @@ namespace GetShorty
 
         public void printGraph(Dictionary<long, LinkedList<Node3>> g)
         {
-            foreach(KeyValuePair<long, LinkedList<Node3>> entry in g)
+            foreach (KeyValuePair<long, LinkedList<Node3>> entry in g)
             {
                 Console.WriteLine(entry.Key + ": ");
                 for (int i = 0; i < entry.Value.Count; i++)
@@ -125,47 +94,6 @@ namespace GetShorty
         }
 
 
-
-        public void dijkstra(Dictionary<long, LinkedList<Node3>> g, Node3 s)
-        {
-            Dictionary<long, double> dist = new Dictionary<long, double>();
-            Dictionary<long, Node3> prev = new Dictionary<long, Node3>();
-
-            foreach(KeyValuePair<long, LinkedList<Node3>> entry in g)
-            {
-                dist.Add(entry.Key, double.MaxValue);
-                prev.Add(entry.Key, null);
-            }
-            dist[s.myName] = 1;
-
-            PriorityQueue pq = new PriorityQueue();
-            pq.insertOrChange(s, 0);
-
-            while(!pq.isEmpty())
-            {
-                Node3 u = pq.deleteMax();
-                LinkedList<Node3> edges = g[u.myName];
-                foreach (Node3 v in edges)
-                {
-                    if (dist[v.myName] > dist[u.myName] * v.factor)
-                    {
-                        dist[v.myName] = dist[u.myName] * v.factor;
-                        prev[v.myName] = u;
-                        pq.insertOrChange(v, dist[v.myName]);
-                    }
-                }
-
-            }
-        }
-
-
-
-
-
-
-
-
-
         public void dijkstra2(Dictionary<long, LinkedList<Node3>> g, long start, long end)
         {
             Dictionary<long, double> dist = new Dictionary<long, double>();
@@ -173,7 +101,7 @@ namespace GetShorty
 
             foreach (KeyValuePair<long, LinkedList<Node3>> entry in g)
             {
-                dist.Add(entry.Key, double.MaxValue);
+                dist.Add(entry.Key, 0.0);
                 prev.Add(entry.Key, null);
             }
             dist[start] = 1.0;
@@ -184,20 +112,29 @@ namespace GetShorty
             while (!pq.isEmpty())
             {
                 Node3 u = pq.deleteMax();
-                LinkedList<Node3> edges = g[u.myName];
+
+                if (Math.Abs(u.factor - dist[u.next]) > 0.000001)
+                    continue;
+
+                LinkedList<Node3> edges = g[u.next];
                 foreach (Node3 v in edges)
                 {
-                    if (dist[v.next] > dist[u.myName] * v.factor)
+                    //if (dist[v.next] > dist[u.myName] * v.factor)
+                    //{
+                    //    dist[v.next] = dist[u.myName] * v.factor;
+                    //    prev[v.next] = u;
+                    //    pq.insertOrChange(v, dist[v.next]);
+                    //}
+                    if (u.factor * v.factor > dist[v.next])
                     {
-                        dist[v.next] = dist[u.myName] * v.factor;
-                        prev[v.next] = u;
-                        pq.insertOrChange(v, dist[v.next]);
+                        dist[v.next] = u.factor * v.factor;
+                        pq.insertOrChange(new Node3(v.next), dist[v.next]);
                     }
                 }
 
             }
-
-            Console.WriteLine("Final answer: " + dist[end]);
+            decimal d = Convert.ToDecimal(dist[end]);
+            Console.WriteLine(string.Format("{0:f4}", d));
         }
 
     }
@@ -214,7 +151,7 @@ namespace GetShorty
 
         public PriorityQueue()
         {
-            
+
         }
 
         public bool isEmpty()
@@ -226,7 +163,7 @@ namespace GetShorty
         {
             double maxVal = -1;
             Node3 maxN = new Node3();
-            foreach(Node3 n in list)
+            foreach (Node3 n in list)
             {
                 if (n.factor > maxVal)
                 {
@@ -239,53 +176,23 @@ namespace GetShorty
             return maxN;
         }
 
+
         public void insertOrChange(Node3 v, double w)
         {
-            if (hash.Contains(v))
-            {
-                v.factor = w;
-            }
-            else
-            {
-                v.factor = w;
-                list.Add(v);
-                hash.Add(v);
-            }
+            //if (hash.Contains(v))
+            //{
+            //    v.factor = w;
+            //}
+            //else
+            //{
+            //    v.factor = w;
+            //    list.Add(v);
+            //    hash.Add(v);
+            //}
+            list.Add(v);
+            v.factor = w;
         }
     }
-
-
-
-
-
-
-
-
-    public class Edge
-    {
-        double factor;
-        Node neighbor;
-
-        public Edge(Node _next, double _factor)
-        {
-            factor = _factor;
-            neighbor = _next;
-        }
-    }
-
-    public class Node
-    {
-        List<Edge> edges;
-        long name;
-        public Node(long _name)
-        {
-            name = _name;
-        }
-    }
-
-
-
-
 
 
 
@@ -302,10 +209,10 @@ namespace GetShorty
         {
             // empty node
         }
-        
-        public Node3(long me)
+
+        public Node3(long n)
         {
-            myName = me;
+            next = n;
         }
 
         public Node3(long me, double _fact)
@@ -322,330 +229,4 @@ namespace GetShorty
         }
     }
 
-
-
-
-
-
-    public class Intersection
-    {
-        long name;
-        public Intersection(long _name)
-        {
-            name = _name;
-        }
-
-        public long getName()
-        {
-            return name;
-        }
-    }
-
-
-    public class Corridor
-    {
-        long factor;
-        Intersection i1;
-        Intersection i2;
-
-        public Corridor(Intersection _i1, Intersection _i2, long _factor)
-        {
-            i1 = _i1;
-            i2 = _i2;
-            factor = _factor;
-        }
-
-        public long getFactor()
-        {
-            return factor;
-        }
-
-        public Intersection getI1()
-        {
-            return i1;
-        }
-
-        public Intersection getI2()
-        {
-            return i2;
-        }
-    }
-
-
-    public class Dungeon
-    {
-        List<Corridor> corridors;
-
-        public Dungeon()
-        {
-            // create a new dungeon
-        }
-
-        public void createCorridor(Corridor c)
-        {
-            corridors.Add(c);
-
-        }
-
-        public Intersection containsIntersection(long name)
-        {
-            foreach (Corridor c in corridors)
-            {
-                if (c.getI1().getName() == name)
-                {
-                    return c.getI1();
-                }
-                else if (c.getI2().getName() == name)
-                {
-                    return c.getI2();
-                }
-            }
-            return null;
-        }
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-    public class Edge2
-    {
-        long end;
-        double factor;
-
-        public Edge2(long _end, double _factor)
-        {
-            end = _end;
-            factor = _factor;
-        }
-
-
-    }
-
-    public class Node2
-    {
-        List<Edge2> neighbors;
-
-        public Node2()
-        {
-
-        }
-
-        public List<Edge2> getNeighbors()
-        {
-            return neighbors;
-        }
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    public class Graph
-    {
-        private List<Vertex> vertice = new List<Vertex>();
-        //private Dictionary<Vertex, Vertex> vertices = new Dictionary<Vertex, Vertex>();
-
-        public Graph()
-        {
-            // don't know what to do here yet
-        }
-
-        public void addVertex(Vertex v)
-        {
-            vertice.Add(v);
-        }
-
-        public void addEdgeToVertex(Vertex v1, Vertex v2)
-        {
-            if (vertice.Contains(v1))
-            {
-                v1.addEdge(v2);
-            }
-        }
-
-        public void drawGraph()
-        {
-            foreach (Vertex v in vertice)
-            {
-                Console.WriteLine("");
-                Console.WriteLine("Vertex " + v.getVertexName() + "(" + v.getPreVisit() + ", " + v.getPostVisit() + ")" + " has edges: ");
-                foreach (Vertex v2 in v.getEdges())
-                {
-                    Console.Write(v2.getVertexName() + "(" + v2.getPreVisit() + ", " + v2.getPostVisit() + ")" + " > ");
-                }
-            }
-        }
-
-        public Vertex getVertexWithName(long s)
-        {
-            foreach (Vertex v in vertice)
-            {
-                if (v.getVertexName().Equals(s))
-                    return v;
-            }
-
-            return null;
-        }
-
-        public List<Vertex> getVertices()
-        {
-            return vertice;
-        }
-    }
-
-    public class Vertex
-    {
-        //private Dictionary<string, int> vertex = new Dictionary<string, int>();
-        private List<Vertex> edges = new List<Vertex>();
-        private long name;
-        private int price;
-        private int dist;
-        private int preVisit;
-        private int postVisit;
-        private bool visited;
-        private long factor;
-        private Vertex prev;
-
-        public Vertex(long _name)
-        {
-            name = _name;
-        }
-
-        public Vertex(long _name, int dist)
-        {
-            name = _name;
-            this.dist = dist;
-        }
-
-        public void addEdge(Vertex v)
-        {
-            edges.Add(v);
-        }
-
-        public List<Vertex> getEdges()
-        {
-            return edges;
-        }
-
-        public long getVertexName()
-        {
-            return name;
-        }
-
-        public void givePreVisit(int num)
-        {
-            preVisit = num;
-        }
-        public int getPreVisit()
-        {
-            return preVisit;
-        }
-
-        public void givePostVisit(int num)
-        {
-            postVisit = num;
-        }
-        public int getPostVisit()
-        {
-            return postVisit;
-        }
-
-        public void setVisited(bool vis)
-        {
-            visited = vis;
-        }
-
-        public bool getVisited()
-        {
-            return visited;
-        }
-
-        public void setPrice(int price)
-        {
-            this.price = price;
-        }
-        public int getPrice()
-        {
-            return price;
-        }
-
-        public void setDist(int dist)
-        {
-            this.dist = dist;
-        }
-        public int getDist()
-        {
-            return dist;
-        }
-
-        public void setPrev(Vertex v)
-        {
-            prev = v;
-        }
-    }
 }
