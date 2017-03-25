4e22e7c867dc3a5bc7ab6de9422a5b267b331b17
McKay Fenn <mckay.fenn@gmail.com>
2017-03-24 18:31:01 -0600

    BankQueue is accepted! :)

--- BankQueue/BankQueue/Program.cs
+++ BankQueue/BankQueue/Program.cs
@@ -12,25 +12,23 @@ namespace BankQueue
         {
             Program p = new Program();
 
-            //string[] testingData = { "4 4", "1000 1", "2000 2", "500 2", "1200 0" };
+            string[] testingData = { "4 4", "1000 1", "2000 2", "500 2", "1200 0" };
             //string[] testingData = { "3 4", "1000 0", "2000 1", "500 1" };
-            string[] testingData = { "4 4", "5 0", "25 1", "10 2", "15 2" };
-
-
-
-            Dictionary<int, List<int>> dict = new Dictionary<int, List<int>>();
-
+            //string[] testingData = { "4 4", "5 0", "25 1", "10 2", "15 2" };
 
             string line;
             int lineCount = 0;
             int timeLeft = 0;
 
 
-            //while ((line = Console.ReadLine()) != null)
-            for (int i = 0; i < testingData.Length; i++)
+            List<Customer> customers = new List<Customer>();
+
+
+            while ((line = Console.ReadLine()) != null)
+            //for (int i = 0; i < testingData.Length; i++)
             {
-                //string[] data = line.Split(null);
-                string[] data = testingData.ElementAt(i).Split(null);
+                string[] data = line.Split(null);
+                //string[] data = testingData.ElementAt(i).Split(null);
 
                 if (lineCount == 0)
                 {
@@ -38,85 +36,91 @@ namespace BankQueue
                     lineCount++;
                     continue;
                 }
-                    
 
-                
+
+
 
                 int amount = Int32.Parse(data[0]);
                 int time = Int32.Parse(data[1]);
 
 
-                if (!dict.ContainsKey(time))
-                {
-                    dict.Add(time, new List<int>());
-                }
-
-                dict[time].Add(amount);   
+                customers.Add(new Customer(time, amount));
 
                 lineCount++;
             }
 
-            //p.printDictionary(dict);
+            // Sort the people by amount
+            Comparison<Customer> comparison = new Comparison<Customer>(compare);
+            customers.Sort(comparison);
 
-            Console.Write(p.solution(dict, timeLeft));
+
+            // create an empty queue
+            List<int> queue = new List<int>(timeLeft);
+            for (int i = 0; i < timeLeft; i++)
+            {
+                queue.Add(0);
+            }
+
+            int k = 0;
+
+            // Put customers into queue (list)
+            while (k < customers.Count)
+            {
+
+
+                Customer c = customers[k];
+                int i = c.waitTime;
+
+                // Pu them into the queue if their time is not already occupied
+                while (i >= 0)
+                {
+                    if (queue[i] == 0)
+                    {
+                        queue[i] = c.amount;
+                        break;
+                    }
+
+                    i--;
+                }
+
+                k++;
+            }
+
+
+            int total = 0;
+
+            for (int i = 0; i < queue.Count; i++)
+            {
+                total += queue[i];
+            }
+
+            Console.WriteLine(total);
 
             Console.ReadLine();
+
         }
 
-
-
-        private int solution(Dictionary<int, List<int>> dict, int time)
+        /// <summary>
+        /// Comparison used for sorting
+        /// </summary>
+        /// <param name="c1"></param>
+        /// <param name="c2"></param>
+        /// <returns></returns>
+        public static int compare(Customer c1, Customer c2)
         {
-            int maxValue = 0;
-            int timeLeft = time;
-            int timeUsed = 0;
-
-          
+            return c2.amount.CompareTo(c1.amount);
         }
+    }
 
-        //private int solution(Dictionary<int, List<int>> dict, int time)
-        //{
-        //    int maxValue = 0;
-        //    int timeLeft = time;
-        //    int timeUsed = 0;
+    class Customer
+    {
+        public int waitTime;
+        public int amount;
 
-        //    for (int i = 0; i < dict.Count; i++)
-        //    {
-        //        if (timeUsed > time)
-        //        {
-        //            break;
-        //        }
-        //        else
-        //        {
-        //            List<int> sorted = new List<int>();
-        //            if (dict.ContainsKey(timeLeft))
-        //            {
-        //                sorted = dict[timeLeft];
-        //                sorted.Sort();
-        //                maxValue += sorted[sorted.Count - 1];
-        //            }
-        //        }
-
-        //        timeLeft--;
-        //        timeUsed++;
-        //    }
-
-        //    return maxValue;
-        //}
-
-
-
-        private void printDictionary(Dictionary<int, List<int>> dict)
+        public Customer(int time, int money)
         {
-            foreach (KeyValuePair<int, List<int>> pair in dict)
-            {
-                Console.Write(pair.Key + ": ");
-                foreach (int amount in pair.Value)
-                {
-                    Console.Write(amount + ", ");
-                }
-                Console.WriteLine();
-            }
+            waitTime = time;
+            amount = money;
         }
     }
 }
